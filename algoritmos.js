2bc5aa189045153ccdd8ccd051090b58e0e3ad96
Caro Ballarini <carolinaballarini@live.com.ar>
2022-07-25 17:18:22 -0300

    agregado de algoritmos

---
+++ algoritmos.js
@@ -0,0 +1,88 @@
+var a;
+var b;
+var c;
+
+function suma (a, b)
+{
+    return c=a + b;
+}
+
+console.log (suma(10,5));
+
+if (c<5)
+{
+    console.log ("el número es menor a 5")
+} 
+else {
+    console.log ("el número es mayor a 5")
+}
+
+
+
+let a;
+let b;
+let c;
+
+function resta (a,b)
+{
+    return c=a - b;
+}
+
+console.log (resta(59,36));
+
+switch(Number) 
+{
+    case 1: 
+    "El número es 30"
+    break;
+    case 2: 
+    "El número es 25"
+    break;
+    case 3:
+    "El número es 23"
+    break;
+    default:
+    "El número es menor a 22"
+}
+
+
+
+
+
+var edad = 20
+if (edad === 18)
+    {
+        console.log ("podes votar");
+    }
+    else if {
+        (edad >18)
+        {console.log ("podes votar de nuevo");}
+    }
+    else {
+        console.log ("aun no podes votar");
+    }
+
+
+
+var a
+var b
+var c
+
+function multiplicación (a,c)
+{
+    return b= a*c;
+}
+console.log (multiplicación(20,6))
+if (b<100)
+{
+    console.log ("el número es menor a 100")
+} 
+else {
+    console.log ("el número es mayor 100")
+}
+
+
+function saludarEstudiante (estudiante)
+{
+    console.log ("hola" + estudiante);
+}
